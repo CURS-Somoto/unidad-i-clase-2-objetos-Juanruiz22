ea1b793dca934698b98ebdfb7c30e66cc5f31ed2
Juanruiz22 <147443872+Juanruiz22@users.noreply.github.com>
2023-11-03 20:36:26 +0100

    Update distancia_puntos.cs
    poo

--- distancia_puntos.cs
+++ distancia_puntos.cs
@@ -1,1 +1,37 @@
+using System;
 
+public class Punto
+{
+    public double x { get; set; }
+    public double y { get; set; }
+    public double z { get; set; }
+
+    public Punto(double x, double y, double z = 0)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public decimal MedirDistancia(Punto destino)
+    {
+       
+        return Math.Sqrt((destino.x - x) * (destino.x - x) + (destino.y - y) * (destino.y - y) + (destino.z - z) * (destino.z - z));
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Punto origen = new Punto(10.0, 20.0);
+        Punto destino = new Punto(50.0, 60.0);
+
+       
+        var distancia = origen.MedirDistancia(destino);
+
+    
+        Console.WriteLine("La distancia entre los dos puntos es: {0}", distancia);
+    }
+}
